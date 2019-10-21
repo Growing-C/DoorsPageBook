c270d1061330b8a9e1c7316cd57d2866637b3cf4
陈高阳 <chengaoyang@linkstec.com>
2019-10-21 18:25:32 +0800

    dev [modify]

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,5 +1,5 @@
 window.onload = function() {
 	console.log('wtf');
-	alert('wtf');
+	//alert('wtf');
   //document.getElementById('main-content').innerHTML = 'Hello, github pages :)'
 }
--- docs/js/main.js
+++ docs/js/main.js
@@ -4,6 +4,7 @@
 
 //判断手机类型
 window.onload = function () {
+	console.log('main.js onload');
     alert($(window).height());
     var u = navigator.userAgent;
     if (u.indexOf('Android') > -1 || u.indexOf('Linux') > -1) {//安卓手机
@@ -30,42 +31,7 @@ var loading_img_url = [
     "./image/0003.jpg",
     "./image/0004.jpg",
     "./image/0005.jpg",
-    "./image/0006.jpg",
-    "./image/0007.jpg",
-    "./image/0008.jpg",
-    "./image/0009.jpg",
-    "./image/0010.jpg",
-    "./image/0011.jpg",
-    "./image/0012.jpg",
-    "./image/0013.jpg",
-    "./image/0014.jpg",
-    "./image/0015.jpg",
-    "./image/0016.jpg",
-    "./image/0017.jpg",
-    "./image/0018.jpg",
-    "./image/0019.jpg",
-    "./image/0020.jpg",
-    "./image/0021.jpg",
-    "./image/0022.jpg",
-    "./image/0023.jpg",
-    "./image/0024.jpg",
-    "./image/0025.jpg",
-    "./image/0026.jpg",
-    "./image/0027.jpg",
-    "./image/0028.jpg",
-    "./image/0029.jpg",
-    "./image/0030.jpg",
-    "./image/0031.jpg",
-    "./image/0032.jpg",
-    "./image/0033.jpg",
-    "./image/0034.jpg",
-    "./image/0035.jpg",
-    "./image/0036.jpg",
-    "./image/0037.jpg",
-    "./image/0038.jpg",
-    "./image/0039.jpg",
-    "./image/0040.jpg",
-    "./image/0041.jpg",
+    "./image/0006.jpg", 
 ];
 
 //加载页面
@@ -79,11 +45,12 @@ function loading() {
         img.src = loading_img_url[i];
         img.onerror = function () {
             numbers += (1 / length) * 100;
+			console.log('onerror:'+numbers);
         }
         img.onload = function () {
             numbers += (1 / length) * 100;
             $('.number').html(parseInt(numbers) + "%");
-            console.log(numbers);
+            console.log('onload:'+numbers);
             if (Math.round(numbers) == 100) {
                 //$('.number').hide();
                 date_end = getNowFormatDate();
@@ -93,10 +60,10 @@ function loading() {
                     //拼接图片
                     $('.shade').hide();
                     var tagHtml = "";
-                    for (var i = 1; i <= 41; i++) {
+                    for (var i = 1; i <= 6; i++) {
                         if (i == 1) {
                             tagHtml += ' <div id="first" style="background:url(image/00' + (i < 10 ? '0' + i : i) + '.jpg) center top no-repeat;background-size:100%"></div>';
-                        } else if (i == 41) {
+                        } else if (i == 6) {
                             tagHtml += ' <div id="end" style="background:url(image/00' + (i < 10 ? '0' + i : i) + '.jpg) center top no-repeat;background-size:100%"></div>';
                         } else {
                             tagHtml += ' <div style="background:url(image/00' + (i < 10 ? '0' + i : i) + '.jpg) center top no-repeat;background-size:100%"></div>';
@@ -137,14 +104,14 @@ function loading() {
                                     $(".btnImg").css("display", "block");
                                     $(".mark").css("display", "none");
                                 }
-                                if (page == 41) {
+                                if (page == 6) {
                                     $(".nextPage").css("display", "none");
                                 } else {
                                     $(".nextPage").css("display", "block");
                                 }
                             },
                             turned: function (e, page, view) {
-                                console.log(page);
+                                console.log('page:'+page);
                                 var total = $(".flipbook").turn("pages");//总页数
                                 if (page == 1) {
                                     $(".return").css("display", "none");
